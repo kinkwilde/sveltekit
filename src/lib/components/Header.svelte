53e195a1475308a66e4e4482a85e3b01ddb9456f
Stephen Wilde <stephen@streetstyleartisans.com>
2024-09-10 15:25:55 +0100

    Add class sorting

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,17 +1,17 @@
 <header class="bg-black text-white">
-	<a href="/">Home</a>
+  <a href="/">Home</a>
 
-	<nav>
-		<ul class="list-none flex gap-4">
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/about">About</a>
-			</li>
-			<li>
-				<a href="/contact">Contact</a>
-			</li>
-		</ul>
-	</nav>
+  <nav>
+    <ul class="flex list-none gap-4">
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
 </header>
